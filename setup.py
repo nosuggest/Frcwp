86d6cbe04bf9d24cfefcab4c47d9e479109ad1aa
sladesha <stw386@sina.com>
2017-12-30 20:48:18 +0800

    update

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='Frcwp',
+    version='0.0.4',
+    description=(
+        'recognize the outliers of the data set'
+    ),
+    author='sladesal',
+    author_email='stw386@sina.com',
+    maintainer='sladesal',
+    maintainer_email='stw386@sina.com',
+    license='MIT License',
+    packages=['Frcwp'],
+    platforms=["all"],
+    url='https://github.com/sladesha',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
